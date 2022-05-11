ad5d93272e40588e1a2ef2c0bce2bdccd4b5d92d
Tiago-Cabral-tech <69585419+Tiago-Cabral-tech@users.noreply.github.com>
2022-05-11 13:08:40 -0300

    Add files via upload

---
+++ utprimers/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://kit.fontawesome.com/ed33a9b95b.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&family=Hurricane&family=Open+Sans:ital,wght@0,300;0,600;1,300&family=Staatliches&family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>utprimers</title>
+	<meta name="descripition" content="descrição do site">
+	<meta name="keywords" content="palavras,chaves,do,site"></head>
+<body>
+	<section class="banner">
+		<div class="logo">
+			<img src="utprimes/logobranca.png">
+		</div><!--logo-->
+		<nav class="desktop">
+			<ul>
+				<li><a>Home</a></li>
+				<li><a>Phytocare</a></li>
+				<li><a>Parceiros</a></li>
+				<li><a>Apoie-nos</a></li>
+			</ul>
+		</nav><!--desktop-->
+		<nav class="mobile">
+			<ul>
+				<li><a>Home</a></li>
+				<li><a>Phytocare</a></li>
+				<li><a>Parceiros</a></li>
+				<li><a>Apoie-nos</a></li>
+			</ul>
+		</nav><!--mobile-->
+
+		<div class="frase">
+			<h2>Search, Development, Inovation </h2>
+			<h3>UTFPR-PG synthetic biology competition team.</h3>
+		</div><!--frase-->
+		<div class="more">
+			<h4>Learn more <i class="fa-solid fa-circle-arrow-down"></i></h4>
+
+		</div><!--frase-->
+		<div class="welcome">
+			<h2>Welcome to the UTPrimers team.</h2>
+			<div class="linha"></div><!--linha-->
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et, faucibus ligula. Etiam at placerat leo. Mauris urna risus, iaculis et leo ac, auctor hendrerit ligula. Integer sed eleifend libero</p>
+		</div><!--welcome-->
+
+		<section class="resumos">
+			<div class="resums vinteecinco">
+				<i class="fa-solid fa-address-card fa-3x"></i>
+				<h2>Sobre</h2>
+				<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+			</div>
+			<div class="resums vinteecinco">
+				<i class="fa-solid fa-sun fa-3x"></i>
+				<h2>Phytocare</h2>
+				<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+			</div>
+			<div class="resums vinteecinco">
+				<i class="fa-solid fa-handshake fa-3x"></i>
+				<h2>Parceiros</h2>
+				<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+			</div>
+			<div class="resums vinteecinco">
+				<i class="fa-solid fa-money-bill-trend-up fa-3x"></i>
+				<h2>Apoie-nos</h2>
+				<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+			</div>
+		</section><!--resumo-->
+		<section class="fotos">
+			<h2>Team</h2>
+			<div class="linha"></div>
+			<div></div>
+		</section><!--fotos-->
+	</section><!--banner-->
+
+	<!--<section class="resumos">
+		<div class="resums vinteecinco">
+			<i class="fa-solid fa-address-card fa-3x"></i>
+			<h2>Sobre</h2>
+			<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+		</div>
+		<div class="resums vinteecinco">
+			<i class="fa-solid fa-sun fa-3x"></i>
+			<h2>Phytocare</h2>
+			<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+		</div>
+		<div class="resums vinteecinco">
+			<i class="fa-solid fa-handshake fa-3x"></i>
+			<h2>Parceiros</h2>
+			<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+		</div>
+		<div class="resums vinteecinco">
+			<i class="fa-solid fa-money-bill-trend-up fa-3x"></i>
+			<h2>Apoie-nos</h2>
+			<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non purus hendrerit, vehicula risus et.</h3>
+		</div>
+	</section>resumo
+	<section class="fotos">
+		<h2>Team</h2>
+		<div class="linha"></div>
+		<div></div>
+	</section>fotos-->
+
+</body>
+</html>
+
+
+<script type="text/javascript">
+	function typeWrite(elemento) {
+		const textoArray = elemento.innerHTML.split('');
+		elemento.innerHTML = '';
+		textoArray.forEach((letra, i) => {
+			setTimeout(function(){
+				elemento.innerHTML += letra;
+			}, 300 * i);
+		});
+	}
+
+	const utprimers = document.querySelector('.frase h2')
+	typeWrite(utprimers)
+</script>
